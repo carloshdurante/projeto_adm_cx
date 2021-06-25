a401ea8fde93a95220c96c8116c8899165e32e7a
Carlos Durante <carlos.durante@outlook.com>
2021-06-25 02:30:42 -0300

    População do banco

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+20.times do |i|
+    fundador = Fundador.create nome: Faker::Name.first_name, sobrenome: Faker::Name.last_name, idade: 20 + i
+    companhia = Company.create!(nome: Faker::Company.name, cnpj: 12345678901234 + i, fundacao: Date.today, fundador_id: fundador.id)
+    lider = Lider.create!(nome: Faker::Name.first_name, sobrenome: Faker::Name.last_name, idade: 27 + i, area: Faker::Job.field, company_id: companhia.id)
+    coordenador = Coordenador.create!(nome: Faker::Name.first_name, sobrenome: Faker::Name.last_name, idade: 30 + i, contratadoem: Date.today, lider_id: lider.id, company_id: companhia.id)
+    empjr = Empjr.create!(nome: Faker::Name.first_name, sobrenome: Faker::Name.last_name, idade: 19 + i, emexperiencia: true, salario: 2500.57, coordenador_id: coordenador.id, company_id: companhia.id)
+    empsr = Empsr.create!(nome: Faker::Name.first_name, sobrenome: Faker::Name.last_name, idade: 24 + i, emexperiencia: true, salario: 8500.57, coordenador_id: coordenador.id, company_id: companhia.id)
+end
+
+
