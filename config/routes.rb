626e132fbd3feb10a45b56f3fd2e050f99ea5b4e
Carlos Durante <carlos.durante@outlook.com>
2021-06-24 22:03:53 -0300

    Adicionado rota para contratação

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      resources :contratacao
       resources :sumario
       resources :lider
       resources :fundador
