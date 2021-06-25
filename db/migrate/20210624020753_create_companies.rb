654506898d398e4c4716c7b68306e857bd49288a
Carlos Durante <carlos.durante@outlook.com>
2021-06-25 02:29:56 -0300

    Alterado type da fundacao para Date

--- db/migrate/20210624020753_create_companies.rb
+++ db/migrate/20210624020753_create_companies.rb
@@ -3,7 +3,7 @@ class CreateCompanies < ActiveRecord::Migration[6.1]
     create_table :companies do |t|
       t.string :nome
       t.integer :cnpj, limit: 14
-      t.string :fundacao
+      t.date :fundacao
       t.references :fundador, null: false, foreign_key: true
 
       t.timestamps
